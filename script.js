b9eb3a0c16a3d92bed7128b5f5ce80cb144b6576
Anh Nguyen <anhnguyeen.ngoc@gmail.com>
2024-08-26 02:36:02 +0700

    get computer choice

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ function getHumanChoice() {
   }
   return input.toLowerCase();
 }
+
+function getComputerChoice() {
+  const random = Math.random();
+  if (0 <= random < 0.33) {
+      console.log("Computer chose: rock");
+      return "rock";
+  }
+  else if (0.33 >= random > 0.66) {
+      console.log("Computer chose: scissors");
+      return "scissors";
+  }
+  else {
+      console.log("Computer chose: paper");
+      return "paper";
+  }
+}
